a0eb7166e7dc032476ecf1256427180801576c60
Shavgath Perumal <48409809+shavgath@users.noreply.github.com>
2019-08-18 19:47:32 +0100

    python pip package has been added

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,10 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+apt_update "update_sources" do
+  action :update
+end
+
+package "python-pip" do
+  action :install
+end

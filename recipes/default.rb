1b7caf09b94792a62882343e3b3f9ec6db23ae53
Shavgath Perumal <48409809+shavgath@users.noreply.github.com>
2019-08-20 11:11:34 +0100

    Added pip plugins

--- recipes/default.rb
+++ recipes/default.rb
@@ -16,5 +16,45 @@ package 'python-pip' do
 end
 
 execute 'python-pip' do
-  command 'pip install -r requirements.txt'
+  command 'pip install Flask==0.10.1'
+end
+
+execute 'python-pip' do
+  command 'pip install Jinja2==2.7.3'
+end
+
+execute 'python-pip' do
+  command 'pip install MarkupSafe==0.23'
+end
+
+execute 'python-pip' do
+  command 'pip install Werkzeug==0.9.6'
+end
+
+execute 'python-pip' do
+  command 'pip install gnureadline==6.3.3'
+end
+
+execute 'python-pip' do
+  command 'pip install itsdangerous==0.24'
+end
+
+execute 'python-pip' do
+  command 'pip install rauth==0.7.0'
+end
+
+execute 'python-pip' do
+  command 'pip install requests==2.3.0'
+end
+
+execute 'python-pip' do
+  command 'pip install wsgiref==0.1.2'
+end
+
+execute 'python-pip' do
+  command 'pip install gunicorn==18.0'
+end
+
+execute 'python-pip' do
+  command 'pip install Flask-SSLify==0.1.4'
 end
